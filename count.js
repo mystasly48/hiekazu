a6009d7536b6a45ff0e813e2f706153b7196ff29
Maisu <mystasly48@gmail.com>
2019-06-08 00:03:32 +0900

    supports countdown forever

--- count.js
+++ count.js
@@ -1,4 +1,4 @@
-// admin: @30msl
+﻿// author: @30msl
 
 count();
 function count(){
@@ -15,19 +15,20 @@ function count(){
   // でもこのコードは本当にひどい
   // マジで反省してます
   // 気が向いたら直します
-  var hiekazu = new Date(2019, 4, 1, 0, 0);
   var now = new Date();
-  if (hiekazu.getTime() < now.getTime()) {
-    document.getElementById("left").innerHTML = "<ruby>冷和<rp>（</rp><rt>ヒエカズ</rt><rp>）</rp></ruby>２年までには";
-    document.getElementById("count").innerHTML = "ドメインが切れます！<br>さようなら！";
-    clearInterval(count);
-  } else {
-    var left = hiekazu - now
-    var days = Math.floor(left / 1000 / 60 / 60 / 24);
-    var hrs = Math.floor((left-days*24*60*60*1000)/1000/60/60);
-    var min = Math.floor((left-days*24*60*60*1000-hrs*60*60*1000)/1000/60);
-    var sec = Math.floor((left-days*24*60*60*1000-hrs*60*60*1000-min*60*1000)/1000);
-    document.getElementById("count").innerHTML = days + "日" + hrs + "時間" + min + "分" + sec + "秒";
-  }
+  var hiekazu = new Date(now.getFullYear()+1, 1, 1, 0, 0);
+  var hieYear = now.getFullYear() - 2018 + 1;
+  var left = hiekazu - now
+  var days = Math.floor(left / 1000 / 60 / 60 / 24);
+  var hrs = Math.floor((left-days*24*60*60*1000)/1000/60/60);
+  var min = Math.floor((left-days*24*60*60*1000-hrs*60*60*1000)/1000/60);
+  var sec = Math.floor((left-days*24*60*60*1000-hrs*60*60*1000-min*60*1000)/1000);
+  var hieText = "";
+  if (days != 0) hieText += days + "日";
+  if (hrs != 0) hieText += hrs + "時間";
+  if (min != 0) hieText += min + "分";
+  if (sec != 0) hieText += sec + "秒";
+  document.getElementById("count").innerHTML = hieText;
+  document.getElementById("hieYear").innerHTML = hieYear;
 }
 setInterval('count()',100);
