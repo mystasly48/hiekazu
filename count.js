16b70a9b31a40d873ec6dc72459be90537be83b3
Maisu <mystasly48@gmail.com>
2019-12-15 23:31:11 +0900

    fixed base of next hiekazu date, feb to jan

--- count.js
+++ count.js
@@ -1,28 +1,18 @@
-﻿// author: @30msl
-
-count();
+﻿count();
 function count(){
-  // え？
-  // ジャヴァスクリプトってこんなに時間処理面倒なの？
-  // マジ？
-  // 僕が無能っていうのは大いに認める
-  // でもこれは許せない
-  // 何がC＃に似ているだ
-  // 僕は絶対に許さない
-  // おいインドネシア
-  // お前もだ
-  // 
-  // でもこのコードは本当にひどい
-  // マジで反省してます
-  // 気が向いたら直します
   var now = new Date();
-  var hiekazu = new Date(now.getFullYear()+1, 1, 1, 0, 0);
+  var hiekazu = new Date(now.getFullYear()+1, 0, 1);
   var hieYear = now.getFullYear() - 2018 + 1;
-  var left = hiekazu - now
-  var days = Math.floor(left / 1000 / 60 / 60 / 24);
-  var hrs = Math.floor((left-days*24*60*60*1000)/1000/60/60);
-  var min = Math.floor((left-days*24*60*60*1000-hrs*60*60*1000)/1000/60);
-  var sec = Math.floor((left-days*24*60*60*1000-hrs*60*60*1000-min*60*1000)/1000);
+  
+  var leftSec = (hiekazu - now)/1000;
+  var days = Math.floor(leftSec/(60*60*24));
+  leftSec -= days*60*60*24;
+  var hrs = Math.floor(leftSec/(60*60));
+  leftSec -= hrs*60*60;
+  var min = Math.floor(leftSec/60);
+  leftSec -= min*60;
+  var sec = Math.floor(leftSec);
+
   var hieText = "";
   if (days != 0) hieText += days + "日";
   if (hrs != 0) hieText += hrs + "時間";
@@ -32,3 +22,4 @@ function count(){
   document.getElementById("hieYear").innerHTML = hieYear;
 }
 setInterval('count()',100);
+
